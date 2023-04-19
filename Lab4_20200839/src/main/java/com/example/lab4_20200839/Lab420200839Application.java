eda2022718070e77a0bdc3fa58777b4ebcd26c7f
DiegoLG15 <a20200839@pucp.edu.pe>
2023-04-18 20:41:03 -0500

    First commit

---
+++ Lab4_20200839/src/main/java/com/example/lab4_20200839/Lab420200839Application.java
@@ -0,0 +1,13 @@
+package com.example.lab4_20200839;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab420200839Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Lab420200839Application.class, args);
+    }
+
+}
---
+++ Lab4_20200839/src/test/java/com/example/lab4_20200839/Lab420200839ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.lab4_20200839;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lab420200839ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
